f7a76934e9d4c0ada8a452ac73941303e9c5c335
Luis J. Gonzalez <jamesqc999@gmail.com>
2023-12-20 10:02:54 -0500

    Add dev and prod configuration

--- main.js
+++ main.js
@@ -1,2 +1,1 @@
-console.log("hello!");
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiQUFBQUEsUUFBUUMsSUFBSSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcImhlbGxvIVwiKTsiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
+console.log("hello with webpack dev server");
