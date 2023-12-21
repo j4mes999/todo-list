f09391bb5b723245405247c2fc638635ee710a48
Luis J. Gonzalez <lgonzalez@sonatype.com>
2023-12-21 15:50:58 -0500

    Add backend, front and persistence layer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import createTask from "./backend/task";
+
 console.log("hello with webpack dev server");
+const task1 = createTask("Do the dishes", "Wash pots, dishes and cutterly","Dic 10","1");
+console.log("task desc: " + task1.description);
