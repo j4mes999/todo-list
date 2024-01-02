3ffc4a36281a02a0f62c31de5d0727f9e735a8e2
Luis J. Gonzalez <jamesqc999@gmail.com>
2024-01-02 10:01:09 -0500

    Add github link in the footer and adjust canvas for it

--- src/frontend/footer.js
+++ src/frontend/footer.js
@@ -1,3 +1,6 @@
+import githubIcon from './media/github.png';
+const MY_GIT_HUB_LINK = 'https://github.com/j4mes999';
+
 function createFooter(){
     const footer = document.createElement('footer');
     footer.classList.add('footer');
@@ -5,7 +8,17 @@ function createFooter(){
     const footerText = document.createElement('p');
     footerText.textContent = `Copyright © ${new Date().getFullYear()} Luis J. González`;
     footerText.classList.add('textFooter');
+
+    const gitHubLink = document.createElement('a');
+    gitHubLink.href = MY_GIT_HUB_LINK;
+    const githubImg = new Image();
+    githubImg.classList.add('githubIcon');
+    githubImg.src = githubIcon;
+    gitHubLink.appendChild(githubImg);
+
+
     footer.appendChild(footerText);
+    footer.appendChild(gitHubLink);
 
     return footer;
 }
