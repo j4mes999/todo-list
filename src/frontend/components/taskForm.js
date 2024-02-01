92ba62c1f19c3eaca3334ae5c309c8a05e78472f
Luis J. Gonzalez <jamesqc999@gmail.com>
2024-02-01 07:55:25 -0500

    Implement close button view task form first version

--- src/frontend/components/taskForm.js
+++ src/frontend/components/taskForm.js
@@ -1,4 +1,4 @@
-import {createElement, createOption, createInput, createButton, createImage} from './htmlElement';
+import { createElement, createOption, createInput, createButton, createImage } from './htmlElement';
 import { Task } from '../constants/uiConstants';
 import { selectedProject } from '../taskpane';
 import serviceCreateTask from '../../backend/service/taskService';
@@ -13,7 +13,7 @@ const PLACE_HOLDER_TITLE = 'Enter Title';
 const PLACE_HOLDER_DESCRIPTION = 'Enter Description';
 const PLACE_HOLDER_DUE_DATE = 'Due Date';
 
-function createTaskForm(){
+function createTaskForm() {
   const createTaskFormContainer = createElement(Task.FORM, Task.FORM);
   const prioritySelect = createOptionList();
   const createButtonElement = createButton(Task.BUTTON_FORM, 'Create', 'submit');
@@ -22,10 +22,10 @@ function createTaskForm(){
   buttonContainer.appendChild(createButtonElement);
   buttonContainer.appendChild(cancelButtonElement);
 
-  createTaskFormContainer.appendChild(createInput(Task.INPUT, PLACE_HOLDER_TITLE ));
+  createTaskFormContainer.appendChild(createInput(Task.INPUT, PLACE_HOLDER_TITLE));
   createTaskFormContainer.appendChild(createInput(Task.INPUT, PLACE_HOLDER_DESCRIPTION));
   const dueDate = createInput(Task.INPUT, PLACE_HOLDER_DUE_DATE);
-  dueDate.onfocus = function(){
+  dueDate.onfocus = function () {
     this.type = 'date';
   };
   dueDate.id = Task.DUE_DATE_ID;
@@ -34,7 +34,7 @@ function createTaskForm(){
   createTaskFormContainer.appendChild(buttonContainer);
 
   cancelButtonAction(cancelButtonElement);
-  createButtonAction(createButtonElement,createTaskFormContainer);
+  createButtonAction(createButtonElement, createTaskFormContainer);
 
   return createTaskFormContainer;
 
@@ -52,7 +52,7 @@ function createOptionList() {
   return prioritySelect;
 }
 
-function cancelButtonAction(cancelButton){
+function cancelButtonAction(cancelButton) {
   cancelButton.addEventListener('click', () => {
     const createTaskForm = document.getElementsByClassName(Task.FORM)[0];
     createTaskForm.classList.toggle(Task.FORM_HIDE);
@@ -62,15 +62,15 @@ function cancelButtonAction(cancelButton){
   });
 }
 
-function createButtonAction(createButton, taskForm){
+function createButtonAction(createButton, taskForm) {
   createButton.addEventListener('click', () => {
     const userInput = document.getElementsByClassName(Task.INPUT);
     const dueDate = document.getElementById(Task.DUE_DATE_ID).value;
     const priority = document.getElementsByClassName(Task.PRIORITY_SELECT)[0];
     const formattedDate = transformDateFormat(dueDate.replace(/-/g, '/'));
-    const task = serviceCreateTask(userInput[0].value,userInput[1].value,formattedDate,priority.value,selectedProject.id);
+    const task = serviceCreateTask(userInput[0].value, userInput[1].value, formattedDate, priority.value, selectedProject.id);
     selectedProject.addTask(task);
-    
+
     const taskPane = document.getElementsByClassName(Task.PANE)[0];
     taskPane.insertBefore(drawTask(task), taskForm);
 
@@ -80,77 +80,92 @@ function createButtonAction(createButton, taskForm){
   });
 }
 
-function drawTask(task){
-  
-  const taskInfo = createElement('span',Task.INFO,task.title);
-  const leftContainer = createElement(Task.LEFT_CONTAINER,Task.LEFT_CONTAINER);
-  leftContainer.appendChild(createImage(Task.INFO_ICON,todoIcon));
+function drawTask(task) {
+
+  const taskInfo = createElement('span', Task.INFO, task.title);
+  const leftContainer = createElement(Task.LEFT_CONTAINER, Task.LEFT_CONTAINER);
+  leftContainer.appendChild(createImage(Task.INFO_ICON, todoIcon));
   leftContainer.appendChild(taskInfo);
-  
-  const rightContainer = createElement(Task.RIGHT_CONTAINER,Task.RIGHT_CONTAINER);
-  const deleteButton = createButton(Task.DELETE_BUTTON, null,'button');
+
+  const rightContainer = createElement(Task.RIGHT_CONTAINER, Task.RIGHT_CONTAINER);
+  const deleteButton = createButton(Task.DELETE_BUTTON, null, 'button');
   deleteButton.value = task.id;
-  const viewDetailsButton = createButton(Task.VIEW_INFO,null, 'button');
+  const viewDetailsButton = createButton(Task.VIEW_INFO, null, 'button');
   viewDetailsButton.value = task.id;
-  const editDetailsButton = createButton(Task.EDIT_INFO,null, 'button');
+  const editDetailsButton = createButton(Task.EDIT_INFO, null, 'button');
   editDetailsButton.value = task.id;
-  editDetailsButton.appendChild(createImage(Task.INFO_ICON,editIcon));
-  deleteButton.appendChild(createImage(Task.INFO_ICON,deleteIcon))
-  viewDetailsButton.appendChild(createImage(Task.INFO_ICON,seeDetailsIcon));
+  editDetailsButton.appendChild(createImage(Task.INFO_ICON, editIcon));
+  deleteButton.appendChild(createImage(Task.INFO_ICON, deleteIcon))
+  viewDetailsButton.appendChild(createImage(Task.INFO_ICON, seeDetailsIcon));
   rightContainer.appendChild(viewDetailsButton);
   rightContainer.appendChild(editDetailsButton);
   rightContainer.appendChild(deleteButton);
 
-  const taskInfoContainer = createElement(Task.INFO_CONTAINER,Task.INFO_CONTAINER);
+  const taskInfoContainer = createElement(Task.INFO_CONTAINER, Task.INFO_CONTAINER);
   taskInfoContainer.id = task.id;
   taskInfoContainer.appendChild(leftContainer);
   taskInfoContainer.appendChild(rightContainer);
 
-  deleteButton.addEventListener('click', function(e) {
+  deleteButton.addEventListener('click', function (e) {
     deleteTaskAction(this.value);
   });
 
-  viewDetailsButton.addEventListener('click', function(e){
-    viewTaskAction(this.value);
-  });
+  viewTaskAction(viewDetailsButton);
 
   return taskInfoContainer;
-  
+
 }
 
-function deleteTaskAction(taskID){
-  deleteTask(selectedProject,taskID);
+function deleteTaskAction(taskID) {
+  deleteTask(selectedProject, taskID);
   document.getElementById(taskID).remove();
 }
 
-function viewTaskAction(taskId){
-  const viewForm = createElement(Task.VIEW_FORM,Task.VIEW_FORM);
-  const task = selectedProject.getTask(taskId);
-  const title = createElement('span', Task.INPUT,task.title);
-  const description = createElement('span', Task.INPUT,task.description);
-  const dueDate = createElement('span', Task.INPUT,task.dueDate);
-  const priority = createElement('span', Task.INPUT,task.priority);
+function viewTaskAction(viewDetailsButton) {
+  viewDetailsButton.addEventListener('click', function (e) {
+    disableAllViewButtons(true);
+    const taskId = this.value;
+    const viewForm = createElement(Task.VIEW_FORM, Task.VIEW_FORM);
+    const task = selectedProject.getTask(taskId);
+    const title = createElement('span', Task.INPUT, task.title);
+    const description = createElement('span', Task.INPUT, task.description);
+    const dueDate = createElement('span', Task.INPUT, task.dueDate);
+    const priority = createElement('span', Task.INPUT, task.priority);
+    const closeButton = createButton(Task.BUTTON_FORM, 'Close', 'button');
+    const buttonContainer = createElement(Task.BUTTON_CONTAINER, Task.BUTTON_CONTAINER);
+    buttonContainer.appendChild(closeButton);
 
-  viewForm.appendChild(title);
-  viewForm.appendChild(description);
-  viewForm.appendChild(dueDate);
-  viewForm.appendChild(priority);
+    viewForm.appendChild(title);
+    viewForm.appendChild(description);
+    viewForm.appendChild(dueDate);
+    viewForm.appendChild(priority);
+    viewForm.appendChild(buttonContainer);
 
-  const taskPane = document.getElementsByClassName(Task.PANE)[0];
-  //taskPane.appendChild(viewForm);
+    const uiContainer = getParentElement(taskId);
+    uiContainer.insertAdjacentElement('afterend', viewForm);
 
-  const uiContainer = getParentElement(taskId);
-  uiContainer.insertAdjacentElement('afterend', viewForm);
+    closeButton.addEventListener('click', () => {
+
+    })
+  });
+
 }
 
-function getParentElement(taskId){
+function disableAllViewButtons(flag){
+  const buttons = document.getElementsByClassName(Task.VIEW_INFO);
+  for(let button of buttons){
+    button.disabled = flag;
+  }
+}
+
+function getParentElement(taskId) {
   const element = document.getElementById(taskId);
-  return element != null ? 
-         element : document.getElementsByClassName(Task.INFO_CONTAINER)[0];
+  return element != null ?
+    element : document.getElementsByClassName(Task.INFO_CONTAINER)[0];
 }
 
-function resetTaskFormFields(){
-  if( typeof document.getElementsByClassName(Task.FORM) !== "undefined"){
+function resetTaskFormFields() {
+  if (typeof document.getElementsByClassName(Task.FORM) !== "undefined") {
     let inputFields = document.getElementsByClassName(Task.INPUT);
     resetInputValue(inputFields[0], PLACE_HOLDER_TITLE);
     resetInputValue(inputFields[1], PLACE_HOLDER_DESCRIPTION);
@@ -161,14 +176,14 @@ function resetTaskFormFields(){
   }
 }
 
-function resetInputValue(inputField, placeHolderValue){
+function resetInputValue(inputField, placeHolderValue) {
   inputField.value = '';
   inputField.placeholder = placeHolderValue;
 }
 
-function enableAddTaskButton(){
+function enableAddTaskButton() {
   const addTaskButton = document.getElementsByClassName(Task.CREATE_BUTTON)[0];
   addTaskButton.disabled = false;
 }
 
-export {createTaskForm, drawTask, resetTaskFormFields};
+export { createTaskForm, drawTask, resetTaskFormFields };
